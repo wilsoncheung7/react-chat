5569bb4a657323f26de17268981d65bcb6b86f20
Mikan <37231830+wilsoncheung7@users.noreply.github.com>
2020-08-22 10:56:22 -0400

    first commit

---
+++ src/views/index.jsx
@@ -0,0 +1,7 @@
+const React = require('react');
+
+function HelloMessage(props) {
+  return <div>Hello {props.name}</div>;
+}
+
+module.exports = HelloMessage;
---
+++ src/views/layouts/default.jsx
@@ -0,0 +1,12 @@
+const React = require('react');
+
+function DefaultLayout(props) {
+  return (
+    <html>
+      <head><title>{props.title}</title></head>
+      <body>{props.children}</body>
+    </html>
+  );
+}
+
+module.exports = DefaultLayout;
